5251402b52b8440048112cd3d6550a043a948a0e
Mathilde Bachy <mathilde_bachy@berkeley.edu>
2021-03-29 13:39:24 +0200

    first react steps

---
+++ admissions-simplified/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Header.css';
+
+class Header extends React.Component {
+    render() {
+      return (
+        <div className="header">
+          <p class="test">test</p>
+        </div>
+      );
+    }
+  }
+  
+  export default Header;
+  // Example usage: <ShoppingList name="Mark" />
