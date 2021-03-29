57a905f862d9361a9324247c90cd0d653171c54b
Mathilde Bachy <mathilde_bachy@berkeley.edu>
2021-03-29 21:40:07 +0200

    student request form and style

--- admissions-simplified/src/components/Student_request.js
+++ admissions-simplified/src/components/Student_request.js
@@ -46,49 +46,53 @@ class Student_request extends React.Component {
 
       return (
         <div className="student_request">
-          <form onSubmit={this.handleSubmit}>
-            <label class="main_input">
-              First Name:
-              <input type="text" name="first_name" value={this.state.first_name} onChange={this.handleChange} />
-            </label>
+          <div class="border">
+            <h1>
+              Student request form
+            </h1>
+            <form onSubmit={this.handleSubmit}>
+              <label >
+                First Name:
+                <input type="text" name="first_name" class="form_containter" value={this.state.first_name} onChange={this.handleChange} />
+              </label>
 
-            <label class="main_input">
-              Last Name:
-              <input type="text" name="last_name" value={this.state.last_name} onChange={this.handleChange} />
-            </label>
+              <label >
+                Last Name:
+                <input type="text" name="last_name" class="form_containter" value={this.state.last_name} onChange={this.handleChange} />
+              </label>
 
-            <label class="main_input">
-              Date of birth:
-              <input type="date" name="date_of_birth" value={this.state.date_of_birth} onChange={this.handleChange} />
-            </label>
+              <label >
+                Date of birth:
+                <input type="date" name="date_of_birth" class="form_containter" value={this.state.date_of_birth} onChange={this.handleChange} />
+              </label>
 
-            <label class="main_input">
-              Document to certify:
-              <select value={this.state.doc_type} name="doc_type" onChange={this.handleChange}>
-                  <option value="degree">Degree</option>
-                  <option value="transcript">Transcript</option>
-                  <option value="certification">Certification</option>
-              </select>
-            </label>
-            {this.state.isTranscript &&
-            <div>
-              <label>
-                Season:
-                <select value={this.state.semester} name="semester" onChange={this.handleChange}>
-                  <option value="fall">Fall</option>
-                  <option value="spring">Spring</option>
+              <label >
+                Document to certify:
+                <select value={this.state.doc_type} name="doc_type" class="form_containter" onChange={this.handleChange}>
+                    <option value="degree">Degree</option>
+                    <option value="transcript">Transcript</option>
+                    <option value="certification">Certification</option>
                 </select>
               </label>
-              <label>
-                Year:
-                <input type="text" name="transcript_year" value={this.state.transcript_year} onChange={this.handleChange} />
-              </label>
-            </div>
-            }
-            <input type="submit" value="Submit" class="main_input"/>
-          </form>
+              {this.state.isTranscript &&
+              <div>
+                <label>
+                  Season:
+                  <select class="form_containter" value={this.state.semester} name="semester" onChange={this.handleChange}>
+                    <option value="fall ">Fall</option>
+                    <option value="spring">Spring</option>
+                  </select>
+                </label>
+                <label>
+                  Year:
+                  <input class="form_containter" type="text" name="transcript_year" value={this.state.transcript_year} onChange={this.handleChange} />
+                </label>
+              </div>
+              }
+              <input type="submit" value="Submit" class="submit"/>
+            </form>
+          </div>
         </div>
-      
       );
     }
   }
