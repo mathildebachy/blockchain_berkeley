9eb8f76f316305862fce48572b520263015a5a18
Mathilde Bachy <mathilde_bachy@berkeley.edu>
2021-03-29 14:33:59 +0200

    Header modif

--- admissions-simplified/src/components/Header.js
+++ admissions-simplified/src/components/Header.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Header.css';
+import logo from './../assets/logo.png'
 
 class Header extends React.Component {
     render() {
       return (
         <div className="header">
-          <p class="test">test</p>
+          <div>
+            <a href="/">
+              <img src={logo} alt="logo"></img>
+              <span>Admissions Simplified</span>
+            </a>
+          </div>
+          
+          <div class="header-buttons">
+            <div>
+              <a href="/">How it works</a>
+            </div>
+            <div>
+              <a href="/">Stats</a>
+            </div>
+          </div>
+
+          <div>
+            <a href="/">Login</a>
+          </div>
         </div>
       );
     }
