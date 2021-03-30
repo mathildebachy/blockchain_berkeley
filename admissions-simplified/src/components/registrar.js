cb115205e125571a100086a1d0bde799cd18458d
Armand Sauzay <armand.sauzay@student.ecp.fr>
2021-03-30 02:22:06 +0200

    added dashboard v1

--- admissions-simplified/src/components/registrar.js
+++ admissions-simplified/src/components/registrar.js
@@ -1,17 +1,153 @@
-import React from 'react'
+import React from 'react';
+import {Row, Col, Card, Table, Tabs, Tab} from 'react-bootstrap';
+
+import Aux from "../hoc/_Aux";
+import DEMO from "../store/constant";
 import Header from './header2.js';
 
-function Univ() {
+import avatar1 from '../assets/images/user/avatar-1.jpg';
+import avatar2 from '../assets/images/user/avatar-2.jpg';
+import avatar3 from '../assets/images/user/avatar-3.jpg';
 
-    return (
-        <div>
-            <Header />
-            <div>
-                <h1>Registrar Dashboard</h1>
-            </div>
-        </div>
-    );
+import './registrar.css';
 
+class Dashboard extends React.Component {
+    render() {
+        const tabContent = (
+            <Aux>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Silje Larsen</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-up f-22 m-r-10 text-c-green"/>3784</span>
+                    </div>
+                </div>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Julie Vad</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-up f-22 m-r-10 text-c-green"/>3544</span>
+                    </div>
+                </div>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar3} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Storm Hanse</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-down f-22 m-r-10 text-c-red"/>2739</span>
+                    </div>
+                </div>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Frida Thomse</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-down f-22 m-r-10 text-c-red"/>1032</span>
+                    </div>
+                </div>
+                <div className="media friendlist-box align-items-center justify-content-center m-b-20">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Silje Larsen</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-up f-22 m-r-10 text-c-green"/>8750</span>
+                    </div>
+                </div>
+                <div className="media friendlist-box align-items-center justify-content-center">
+                    <div className="m-r-10 photo-table">
+                        <a href={DEMO.BLANK_LINK}><img className="rounded-circle" style={{width: '40px'}} src={avatar3} alt="activity-user"/></a>
+                    </div>
+                    <div className="media-body">
+                        <h6 className="m-0 d-inline">Storm Hanse</h6>
+                        <span className="float-right d-flex  align-items-center"><i className="fa fa-caret-down f-22 m-r-10 text-c-red"/>8750</span>
+                    </div>
+                </div>
+            </Aux>
+        );
+
+        return (
+            <Aux>
+                <Row className='background'>
+                <Header />
+                    <Col md={6} xl={8}>
+                        <Card className='Recent-Users'>
+                            <Card.Header>
+                                <Card.Title as='h1'>Pending Approvals</Card.Title>
+                            </Card.Header>
+                            <Card.Body className='px-0 py-2'>
+                                <Table responsive hover>
+                                    <tbody>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Isabella Christensen</h6>
+                                            <p className="m-0">Bachelor of Science</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15"/>11 MAY 12:56</h6>
+                                        </td>
+                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
+                                    </tr>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Mathilde Andersen</h6>
+                                            <p className="m-0">Master of Financial Engineering</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted"><i className="fa fa-circle text-c-red f-10 m-r-15"/>11 MAY 10:35</h6>
+                                        </td>
+                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
+                                    </tr>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar3} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Karla Sorensen</h6>
+                                            <p className="m-0">Bachelor of Arts</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15"/>9 MAY 17:38</h6>
+                                        </td>
+                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
+                                    </tr>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar1} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Ida Jorgensen</h6>
+                                            <p className="m-0">Bachelor of Science</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted f-w-300"><i className="fa fa-circle text-c-red f-10 m-r-15"/>19 MAY 12:56</h6>
+                                        </td>
+                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
+                                    </tr>
+                                    <tr className="unread">
+                                        <td><img className="rounded-circle" style={{width: '40px'}} src={avatar2} alt="activity-user"/></td>
+                                        <td>
+                                            <h6 className="mb-1">Albert Andersen</h6>
+                                            <p className="m-0">Master of Engineering</p>
+                                        </td>
+                                        <td>
+                                            <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15"/>21 July 12:56</h6>
+                                        </td>
+                                        <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Reject</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Approve</a></td>
+                                    </tr>
+                                    </tbody>
+                                </Table>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Aux>
+        );
+    }
 }
 
-export default Univ;
+export default Dashboard;
