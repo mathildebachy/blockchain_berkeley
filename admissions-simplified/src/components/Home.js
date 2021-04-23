f939751aaa0c3a6c495368f5921b8c1f3720b91f
mathilde_bachy <mathilde_bachy@berkeley.edu>
2021-04-23 20:06:27 +0200

    redirection bugfix

--- admissions-simplified/src/components/Home.js
+++ admissions-simplified/src/components/Home.js
@@ -4,6 +4,7 @@ import './Home.css';
 import digital_icon from './../assets/digital_icon.png'
 import {Link} from "react-router-dom";
 import { UserContext } from '../providers/UserProvider'
+import { UpdateTwoTone } from '@material-ui/icons';
 
 const Home = () => {
     const user = useContext(UserContext);
@@ -14,12 +15,12 @@ const Home = () => {
           <h1>Transcripts and Degrees on Blockchain</h1>
           <h3>Forgery-proof and 100% digital certificates: more than 100 institutions and 15 countries trust in us to issue blockchain digital credentials</h3>
         </div>
-        {user.userType === "student"
+        {!user 
         ?
         <div class="home_buttons">
-          <Link to='/student-request'>
+          <Link to="/sign-in">
             <button class="home_button">
-                <p>Get Started Now</p>
+                <p>Get started now</p>
             </button>
           </Link>
           <Link to='/meet-the-team' className='text-link'>
@@ -28,19 +29,49 @@ const Home = () => {
             </button>
           </Link>
         </div>
-        : 
-        <div class="home_buttons">
-          <Link to={user.userType+"-dashboard"}>
-            <button class="home_button">
-                <p>To Dashboard</p>
-            </button>
-          </Link>
-          <Link to='/meet-the-team' className='text-link'>
-            <button class="home_button">
-              <p>Meet the team</p>
-            </button>
-          </Link>
-        </div>
+        : (user.userType === "student"
+          ?
+          <div class="home_buttons">
+            <Link to='/student-request'>
+              <button class="home_button">
+                  <p>Get Started Now</p>
+              </button>
+            </Link>
+            <Link to='/meet-the-team' className='text-link'>
+              <button class="home_button">
+                <p>Meet the team</p>
+              </button>
+            </Link>
+          </div>
+          : (user.userType === "highschool"
+            ?
+              <div class="home_buttons">
+                <Link to={"registrar-dashboard"}>
+                  <button class="home_button">
+                      <p>To Dashboard</p>
+                  </button>
+                </Link>
+                <Link to='/meet-the-team' className='text-link'>
+                  <button class="home_button">
+                    <p>Meet the team</p>
+                  </button>
+                </Link>
+              </div>
+            :
+              <div class="home_buttons">
+                <Link to={"university-dashboard"}>
+                  <button class="home_button">
+                      <p>To Dashboard</p>
+                  </button>
+                </Link>
+                <Link to='/meet-the-team' className='text-link'>
+                  <button class="home_button">
+                    <p>Meet the team</p>
+                  </button>
+                </Link>
+              </div>
+            ) 
+          )
         }
       </div>
     );
